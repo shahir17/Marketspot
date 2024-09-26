546bbc162e079186a12de9c48bc8aa32017daea5
Ahmad Shahir Abdul Satar <abdulsataas2@vcu.edu>
2024-09-26 17:51:30 -0400

    Initial Commit

---
+++ Marketspot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Marketspot
+//
+//  Created by Ahmad Shahir Abdul Satar on 9/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Marketspot/MarketspotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketspotApp.swift
+//  Marketspot
+//
+//  Created by Ahmad Shahir Abdul Satar on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MarketspotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
